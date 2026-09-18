60813506d386715225824903f1c31f43109110f7
Aythami Hernández Bascuñana <aythami19.hb@gmail.com>
2026-09-18 17:24:33 +0200

    Crear clase Persona con atributos básicos

---
+++ P1A1_Aythami_Nacho_Projecto/P1A1_Aythami_Nacho_Projecto/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1A1_Aythami_Nacho_Projecto
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
