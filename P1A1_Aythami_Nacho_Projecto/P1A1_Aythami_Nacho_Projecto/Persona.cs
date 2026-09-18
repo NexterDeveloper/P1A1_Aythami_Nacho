759a266bdfe283543fbc29227be69e76d6b137db
Aythami Hernández Bascuñana <aythami19.hb@gmail.com>
2026-09-18 17:30:50 +0200

    Crear constructor de Persona

--- P1A1_Aythami_Nacho_Projecto/P1A1_Aythami_Nacho_Projecto/Persona.cs
+++ P1A1_Aythami_Nacho_Projecto/P1A1_Aythami_Nacho_Projecto/Persona.cs
@@ -8,5 +8,11 @@ namespace P1A1_Aythami_Nacho_Projecto
     {
         public string Nombre { get; set; }
         public int Edad { get; set; }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
     }
 }
