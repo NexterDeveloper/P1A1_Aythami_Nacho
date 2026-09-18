1cc22b7d2efa2f726f886101857d70ffec6b7044
nacho27bs <nachobs27@gmail.com>
2026-09-18 17:35:00 +0200

    Añadido constructor de Producto

--- P1A1_Aythami_Nacho_Projecto/P1A1_Aythami_Nacho_Projecto/Program.cs
+++ P1A1_Aythami_Nacho_Projecto/P1A1_Aythami_Nacho_Projecto/Program.cs
@@ -2,4 +2,10 @@
 {
     public string Nombre { get; set; }
     public double Precio { get; set; }
+
+    public Producto(string nombre, double precio)
+    {
+        Nombre = nombre;
+        Precio = precio;
+    }
 }
